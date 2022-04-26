1b9d6ed508dd65b58a2ecc8a76d5710b563b6d18
Mqalliu <101127787+Mqalliu@users.noreply.github.com>
2022-04-26 12:50:49 +0200

    Update app_tts.py

--- app_tts.py
+++ app_tts.py
@@ -2,33 +2,21 @@
 
 from googletrans import Translator
 from gtts import gTTS
-import IPython.display as ipd
+import streamlit as st
 
-def audioplayer(audio):
-   ipd.display(ipd.Audio(audio, autoplay=False))
-
-# It asks the user to type in some text
-
-text = input("Give me some text you want me to translate in English and read for you: ")
+user_text = st.text_input("Give me some text you want me to translate in English and read for you: ")
 
 # It then converts that text in a certain langauge
 
 translator = Translator()
-
-text_to_translate = translator.translate(text, dest='en')
-
-#print(text_to_speech.text)
+text_to_translate = translator.translate(text=user_text, dest='en')
 
 text_to_speech = text_to_translate.text
-
 # it converts the translated text to speech.
-
 tts=gTTS(text=text_to_speech, lang='en')
 
 tts.save('audio.mp3')
 
-
-
-print("Your text would sound like this in English:")
-
-audioplayer('audio.mp3')
+st.write("Your text would sound like this in English:")
+audio_file = open('audio.mp3', 'rb')
+st.audio(data=audio_file, format="audio/mp3", start_time=0)
